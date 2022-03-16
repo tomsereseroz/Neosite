01b7cd058f7a8b477c79b6798ad42d466c63de94
TomSereseroz <tomsereseroz@gmail.com>
2022-03-16 19:54:27 -0400

    starting work on navbar links

---
+++ portfolio.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<?php include('header.html'); ?>
+<body>
+	<?php include('navbar.html'); ?>
+	<style>
+		#portfolio a{
+			color: black;
+		}
+	</style>
+<main>
+    <p>Hello World!</p>
+</main></body></html>
